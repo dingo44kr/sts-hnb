41fcb6a30ee36af8f2db34ac6ea49721a32432fc
HB <HB@HB-PC>
2015-12-10 19:40:58 +0900

    2015.12.10 last

--- hnb/src/main/java/com/hnb/main/MainController.java
+++ hnb/src/main/java/com/hnb/main/MainController.java
@@ -18,23 +18,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class MainController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MainController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET) 
 	// value에는 url을 입력하여, jsp에서 할당한 주소와 동일한 주소를 준 이곳으로 들어올 수 있도록 하는 역할을 함.
-	public String home(Locale locale, Model model) { //여기서 모델은 response, request의 역할을 모두 포함한다.
-		
-		logger.info("메인컨트롤러 home 메서드 진입 {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		return "global/Main.tiles"; //어느 경로로 갈것인지를 결정함 
+	public String home() { //여기서 모델은 response, request의 역할을 모두 포함한다.
+		return "global/default.tiles"; //어느 경로로 갈것인지를 결정함 
 	} // 지금 현재 위의 경로는 예전의 소스로 보자면,  /WEB-INF/tiles.xml + global/Main.tiles 인데 어째서 /WEB-INF/tiles.xml로 바로 가는지 이해가 안감.
 	
 	
--- hnb/src/main/java/com/hnb/movie/MovieController.java
+++ hnb/src/main/java/com/hnb/movie/MovieController.java
@@ -26,7 +26,7 @@ public class MovieController {
 	@RequestMapping("/Movie")
 	public String movie() {
 		logger.info("MovieController-home() 진입");
-		return "movie/Movie.tiles";
+		return "movie/Movie.jsp";
 	}
 	
 	@RequestMapping("/movie_info")
